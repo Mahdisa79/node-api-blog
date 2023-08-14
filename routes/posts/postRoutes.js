f4952914d0d9d2660370c1ee27fb87d4bd820f59
Mahdi sadeghi <mahdi.sadeghi1915@gmail.com>
2023-08-14 12:48:29 +0330

    first commit

---
+++ routes/posts/postRoutes.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const storage = require("../../config/cloudinary");
+const multer = require("multer");
+const {
+  postDetailsCtrl,
+  createpostCtrl,
+  deletepostCtrl,
+  updatepostCtrl,
+  fetchPostsCtrl,
+  toggleLikesPostCtrl,
+  toggleDisLikesPostCtrl,
+} = require("../../controllers/posts/postCtrl");
+const isLogin = require("../../middlewares/isLogin");
+
+const postRouter = express.Router();
+
+//file upload middleware
+const upload = multer({ storage });
+
+//POST/api/v1/posts
+// postRouter.post("/", isLogin, upload.single("image"), createpostCtrl);
+postRouter.post("/", isLogin , createpostCtrl);
+
+
+//GET/api/v1/posts/:id
+postRouter.get("/:id", isLogin, postDetailsCtrl);
+
+//GET/api/v1/posts/likes/:id
+postRouter.get("/likes/:id", isLogin, toggleLikesPostCtrl);
+
+//GET/api/v1/posts/dislikes:id
+postRouter.get("/dislikes/:id", isLogin, toggleDisLikesPostCtrl);
+
+//GET/api/v1/posts
+postRouter.get("/", fetchPostsCtrl);
+
+//DELETE/api/v1/posts/:id
+postRouter.delete("/:id", isLogin, deletepostCtrl);
+
+//PUT/api/v1/posts/:id
+postRouter.put("/:id", isLogin, upload.single("image"), updatepostCtrl);
+
+module.exports = postRouter;
